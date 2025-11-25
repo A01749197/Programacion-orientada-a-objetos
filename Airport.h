6129ef57024d32f9cecfeb218520be522077b14b
David López Correa <146491908+A01749197@users.noreply.github.com>
2025-11-24 22:40:33 -0600

    Replace pragma once with include guards in Airport.h

--- Airport.h
+++ Airport.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef AIRPORT_H
+#define AIRPORT_H
+
 #include <iostream>
 #include "Airline.h"
 using namespace std;
@@ -9,30 +11,40 @@ class Airport
 {
 private:
 	Airline airlines[MAXAIRLINE];
-	Flight allFlights[MAXFLIGHT];
 	string name;
 	string currentdate;
 	string city;
 	string country;
 	int totaltransactions;
 	double totaldailyincome;
+
 public:
 	Airport();
-	void setairlines(int index, Airline a);
+	
+    void setairlines(int index, Airline a);
 	Airline* getairlines();
-	void setname(string name);
+	
+    void setname(string name);
 	string getname();
-	void setcurrentdate(string date);
+	
+    void setcurrentdate(string currentdate);
 	string getcurrentdate();
-	void setcity(string city);
+	
+    void setcity(string city);
 	string getcity();
-	void setcountry(string country);
+	
+    void setcountry(string country);
 	string getcountry();
-	void settotaltransactions(int totaltransactions);
+	
+    void settotaltransactions(int totaltransactions);
 	int gettotaltransactions();
-	void settotaldailyincome(double totaldailyincome);
+	
+    void settotaldailyincome(double totaldailyincome);
 	double gettotaldailyincome();
-	void startoperations(string currentdate);
+	
+    void startoperations(string currentdate);
 	void closeoperations();
 	void printallflights();
 };
+
+#endif
